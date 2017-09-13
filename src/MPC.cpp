88ce7ee307b7f5fa21f2db03365b8a53e9677a71
Fahad Zubair <fahadzubair@gmail.com>
2017-09-13 22:48:40 +0500

    letting go of constraints on latency

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -3,11 +3,11 @@
 #include <cppad/ipopt/solve.hpp>
 #include "Eigen-3.3/Eigen/Core"
 
-//#define _USE_LATENCY_CONSTRAINT
+#define _USE_LATENCY_CONSTRAINT
 
 using CppAD::AD;
 
-size_t N = 25;
+size_t N = 26;
 double dt = 0.1;
 
 // Both the reference cross track and orientation errors are 0.
@@ -25,6 +25,7 @@ size_t cte_start = v_start + N;
 size_t epsi_start = cte_start + N;
 size_t delta_start = epsi_start + N;
 size_t a_start = delta_start + N - 1;
+size_t latency_constraint_start = delta_start;
 
 class FG_eval {
  public:
@@ -34,6 +35,9 @@ class FG_eval {
 
   typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
   void operator()(ADvector& fg, const ADvector& vars) {
+
+    cout << "FG Size: " << fg.size() << endl;
+    cout << "Vars Size: " << vars.size() << endl;
 
     // The cost is stored is the first element of `fg`.
     // Any additions to the cost should be added to `fg[0]`.
@@ -79,8 +83,8 @@ class FG_eval {
     fg[1 + epsi_start] = vars[epsi_start];
 
     #ifdef _USE_LATENCY_CONSTRAINT
-    fg[1 + delta_start] = vars[delta_start];
-    fg[1 + a_start] = vars[a_start];
+    fg[1 + latency_constraint_start] = vars[delta_start];
+    fg[1 + latency_constraint_start + 1] = vars[a_start];
     #endif
 
     // The rest of the constraints
@@ -101,7 +105,6 @@ class FG_eval {
       AD<double> cte0 = vars[cte_start + t - 1];
       AD<double> epsi0 = vars[epsi_start + t - 1];
 
-      // Only consider the actuation at time t.
       AD<double> delta0 = vars[delta_start + t - 1];
       AD<double> a0 = vars[a_start + t - 1];
 
@@ -130,19 +133,21 @@ class FG_eval {
 
       // put in a constraint that the acceleration and steering for every alternate
       // time T won't be changed
-#ifdef _USE_LATENCY_CONSTRAINT
-      // for every other 100ms we need to make sure that there is a constraint
-      // to use the same value as before
-
-      if (t % 2 == 1) {
-        AD<double> delta1 = vars[delta_start + t];
-        AD<double> a1 = vars[a_start + t];
-
-        cout << "Constraint @: " << 1 + delta_start + t - 1 << " for time: " << t << endl;
-        fg[1 + delta_start + t - 1] = delta1 - delta0;
-        fg[1 + delta_start + t] = a1 - a0;
-      }
-#endif
+//#ifdef _USE_LATENCY_CONSTRAINT
+//      // for every other 100ms we need to make sure that there is a constraint
+//      // to use the same value as before
+//
+//      if (t % 2 == 2) {
+//        //cout << "About to access: " << delta_start + t << " and " << a_start + t << endl;
+//
+//        AD<double> delta1 = vars[delta_start + t];
+//        AD<double> a1 = vars[a_start + t];
+//
+//        //cout << "Constraint @: " << 1 + delta_start + t - 1 << " for time: " << t << endl;
+//        fg[1 + latency_constraint_start + t + 1] = delta1 - delta0;
+//        fg[1 + latency_constraint_start + t + 2] = a1 - a0;
+//      }
+//#endif
     }
 
     cout << "Going out" << endl;
@@ -176,19 +181,21 @@ Result MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   double epsi = state[5];
 
   size_t n_vars = N * 6 + (N - 1) * 2;
+  size_t n_constraints = N * 6;
 
 #ifdef _USE_LATENCY_CONSTRAINT
   double delta = state[6];
   double a = state[7];
 
-  // The value set for delta and acceleration will remain the same
-  // for the next 100ms, then it can change and then again for the next
-  // 100ms the value cannot change. Therefore there will be
-  // ((N - 1) / 2) * 2 more constraints.
+  // we need to add constraint for every other 100ms to enforce that steering and acceleration
+  // has to be the same as before
 
-  size_t n_constraints = N * 6 + (N - 1);
+  assert(N % 2 == 0);
+
+  //double extra = N + 1;
+  //n_constraints += extra;
+  n_constraints += 2;
 #else
-  size_t n_constraints = N * 6;
 #endif
 
   Dvector vars(n_vars);
@@ -246,10 +253,10 @@ Result MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   constraints_lowerbound[epsi_start] = epsi;
 
 #ifdef _USE_LATENCY_CONSTRAINT
-  constraints_lowerbound[delta_start] = delta;
-  constraints_upperbound[delta_start] = delta;
-  constraints_lowerbound[a_start] = a;
-  constraints_upperbound[a_start] = a;
+  constraints_lowerbound[latency_constraint_start ] = delta;
+  constraints_upperbound[latency_constraint_start ] = delta;
+  constraints_lowerbound[latency_constraint_start  + 1] = a;
+  constraints_upperbound[latency_constraint_start  + 1] = a;
 #endif
 
   constraints_upperbound[x_start] = x;
